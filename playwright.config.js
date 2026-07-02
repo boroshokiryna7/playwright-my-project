effa5fe11533951efafef59118a82e4912362490
IrynaFal <boroshok.iryna7@gmail.com>
2026-07-02 15:50:53 +0200

    latest changes for ht 31.1.2

--- playwright.config.js
+++ playwright.config.js
@@ -33,7 +33,7 @@ module.exports = defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   use: {
     baseURL: process.env.BASE_URL,
     httpCredentials: {
